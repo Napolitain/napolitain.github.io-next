9c6d0ba891f07df68b46001472dc98535bc50436
Napolitain <Napolitain@users.noreply.github.com>
2022-04-10 20:46:45 +0000

    deploy: bd97276d4832e5195bf2e4ae039b16472e512020

---
+++ _next/static/yCjTs18d7_QByUQJp7dXZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-8510385294016c30.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-c4b7b097c50e0e5a.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
