a4ee38588702793443488df4f64687a6ea2fa526
Maxime Boucher <mxboucher@gmail.com>
2022-04-10 21:59:05 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   assetPrefix: isProd ? "/napolitain.github.io/" : "",
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
 
 module.exports = nextConfig
