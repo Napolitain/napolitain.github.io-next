d193a2a65ade50e5efa17f5943fb5ebc95d085b3
Maxime Boucher <mxboucher@gmail.com>
2022-04-10 16:28:54 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/lab34.github.io/" : "",
 }
 
 module.exports = nextConfig
