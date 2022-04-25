7c929ce16812650ec838825f67cd1392a744f4ea
Maxime Boucher <mxboucher@gmail.com>
2022-04-26 00:18:37 +0200

    update

--- components/playground.tsx
+++ components/playground.tsx
@@ -5,26 +5,36 @@ import styles from '../styles/Home.module.css'
 
 const Playground: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        Maxime Boucher
-      </h1>
+    <div className={styles.grid}>
+    <a href="https://nextjs.org/docs" className={styles.card}>
+      <h2>Documentation &rarr;</h2>
+      <p>Find in-depth information about Next.js features and API.</p>
+    </a>
 
-      <p className={styles.description}>
-        Développeur, architecte et chef de projet
+    <a href="https://nextjs.org/learn" className={styles.card}>
+      <h2>Learn &rarr;</h2>
+      <p>Learn about Next.js in an interactive course with quizzes!</p>
+    </a>
+
+    <a
+      href="https://github.com/vercel/next.js/tree/canary/examples"
+      className={styles.card}
+    >
+      <h2>Examples &rarr;</h2>
+      <p>Discover and deploy boilerplate example Next.js projects.</p>
+    </a>
+
+    <a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      className={styles.card}
+    >
+      <h2>Deploy &rarr;</h2>
+      <p>
+        Instantly deploy your Next.js site to a public URL with Vercel.
       </p>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-      </div>
-    </div>
+    </a>
+  </div>
   )
 }
+
+export default Playground
